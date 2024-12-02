e356853cf26a7742ecad2c8e4a3ab8be2ebd2ec7
Yeloo1 <164965791+Yeloo1@users.noreply.github.com>
2024-12-02 22:49:13 +0800

    Update SignUpForm.vue

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="signup-container">
-    <h1 class="title">Sign up</h1>
+    <h1 class="title">Sign up11</h1>
     
     <form class="signup-form" @submit.prevent="signUp">
       <!-- User Input -->
